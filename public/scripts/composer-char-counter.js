9d0e8065293d32dcecc55b8492f98ebdc49fa9c5
code-driving <tittan7@gmail.com>
2021-03-25 19:59:06 +0000

    Format the code

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,5 +1,4 @@
 $(document).ready(function () {
-  
   $(".tweet-compose").on("keyup", function () {
     const charCount = $(this).val().length;
 
@@ -15,6 +14,25 @@ $(document).ready(function () {
         .find(".counter")
         .addClass("negative-values")
         .text(140 - charCount);
+    }
+  });
+});
+
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if ($(this).scrollTop()) {
+      $(".scroll").fadeIn();
+      $("nav button, .arrow").hide();
+    } else {
+      $(".scroll").fadeOut();
+      $("nav button, .arrow").show();
+    }
+  });
+  $(".scroll").on("click", function () {
+    $(this).css("border", "none");
+    window.scrollTo(0, 0);
+    if ($(".new-tweet").is(":hidden")) {
+      $(".new-tweet").show();
     }
   });
 });
